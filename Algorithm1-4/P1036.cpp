f299e3ddebcd4035102eb51f9a4830a7e741aa5d
Chan <fishlikeorangesunny@outlook.com>
2021-04-04 16:30:11 +0800

    Apr. 4

--- Algorithm1-4/P1036.cpp
+++ Algorithm1-4/P1036.cpp
@@ -1,25 +1,51 @@
-#include <stdio.h>
-int a[100000000];//在主函数外定义全局变量数值上限高，自行修改到3、4亿也行，主函数里可定义不到这么大
-int main()
-{
-	int n,i,k,v=0;
-	scanf("%d",&n);
-	for(i=0;i<n;i++)
-	{
-		a[i]=i+1;
-	}
-	for(i=1;i<n;i++)
-	{
-		if(a[i]!=0)//晒过后的第一个非零值一定是素数
-		{
-			for(k=2;k*(i+1)<=n;k++)
-			{
-				a[k*(i+1)-1]=0;//将a[i]的整数倍筛掉
-			}
-			//printf("%d、",a[i]);打印太慢，这里屏蔽了
-			v++;		
-		}
-	}
-	printf("共%d个素数\n",v);
-	return 0;
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int x[25];
+int pos[25];
+long Count = 0;
+
+inline void PickNum(int, int, int, int);
+inline void IsPrime(int);
+
+int main () {
+    ios::sync_with_stdio(0);
+
+    int n, k;
+    cin >> n >> k;
+    for(int i = 0; i < n; ++i) {
+        cin >> x[i];
+    }
+
+    PickNum(0, n - 1, k, 0);
+    cout << Count << endl;
+
+    return 0;
+}
+
+inline void PickNum(int from, int to, int num, int flag) {
+    if(num == 0) {
+        int m = 0;
+        for(int i = 0; i < flag; ++i) {
+            m += x[pos[i]];
+        }
+        IsPrime(m);
+
+        return ;
+    }
+
+    for(int i = from; to - i + 1 >= num; ++i) {
+        pos[flag] = i;
+        PickNum(i + 1, to, num - 1, flag + 1);
+    }
+}
+
+inline void IsPrime(int x) {
+    int max = sqrt(x);
+    for(int i = 2; i <= max; ++i) {
+        if(x % i == 0) return ;
+    }
+    Count++;
+    return ;
 }
